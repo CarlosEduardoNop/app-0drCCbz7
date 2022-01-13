60a17f36dc419e1606bce78f407b8d07d5c21182
CarlosEduardoNop <CarlosEduardoS.C@hotmail.com>
2022-01-13 20:17:50 -0300

    Adição da função store

---
+++ app/Http/Controllers/ProdutoController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Produtos;
+use Illuminate\Support\Facades\Hash;
+
+class ProdutoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Produtos::all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $newProduto = new Produtos();
+        if ($request->nome == null or ''){
+            return 'Nome inválido';
+        };
+        if ($request->quantidade <= 0 or null){
+            return 'Quantidade inválida';
+        };
+        $sku = bin2hex(random_bytes(12));
+        $newProduto->nome = $request->nome;
+        $newProduto->sku = $sku;
+        $newProduto->quantidade = $request->quantidade;
+        $newProduto->save();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProdutoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/produtos', [\App\Http\Controllers\ProdutoController::class, 'index']);
+Route::prefix('/produto')->group( function () {
+        Route::post('/store', [ProdutoController::class, 'store']);
+        Route::put('/{id}', [ProdutoController::class, 'update']);
+        Route::delete('{id}', [ProdutoController::class, 'destroy']);
+    }
+);
